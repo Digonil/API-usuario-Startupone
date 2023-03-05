696595d9ddf21b9c093d9455a10cbb518f17f808
digonil <diego.niles@hotmail.com>
2023-03-05 01:16:44 -0300

    Adicionado validações no DTO de cadastro de Usuarios

--- src/main/java/API/Usuario/startupone/usuario/CadastroUsuariosDto.java
+++ src/main/java/API/Usuario/startupone/usuario/CadastroUsuariosDto.java
@@ -1,16 +1,26 @@
 package API.Usuario.startupone.usuario;
 
 import API.Usuario.startupone.endereco.DadosEnderecoDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record CadastroUsuariosDto(
-      
+        @NotBlank
         String nome,
+        @NotBlank
         String sobrenome,
+        @Email
         String email,
+        @NotBlank
         String rg,
+        @NotBlank
         String cpf,
+        @NotBlank
         String telefoneResidencial,
+        @NotBlank
         String telefoneCelular,
+        @Valid
         DadosEnderecoDto endereco) {
 }
 
